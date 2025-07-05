e1a93bdd5a5504c6796dfed3f1ac80135ea69001
Salam <salam46.khan@gmail.com>
2025-07-05 22:41:51 +0600

    add cors

--- dist/app.js
+++ dist/app.js
@@ -4,12 +4,14 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
+const cors_1 = __importDefault(require("cors"));
 const book_controller_1 = require("./app/controllers/book.controller");
 const borrow_controller_1 = require("./app/controllers/borrow.controller");
 const app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use("/books", book_controller_1.booksRouter);
-app.use("/borrow", borrow_controller_1.borrowRouter);
+app.use((0, cors_1.default)());
+app.use("/api/books", book_controller_1.booksRouter);
+app.use("/api/borrow", borrow_controller_1.borrowRouter);
 app.get('/', (req, res) => {
     res.send('Library management system');
 });
